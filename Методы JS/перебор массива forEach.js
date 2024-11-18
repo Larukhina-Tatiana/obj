a837e57250a1c2846a5e6c1cdac1f814e8519ac2
Татьяна <ltasya72@gmail.com>
2024-11-18 20:47:54 +0200

    js

---
+++ Методы JS/перебор массива forEach.js
@@ -0,0 +1,14 @@
+// Перебор элементов массива
+
+// Метод forEach
+
+let arr = ["Юля", "Ваня", "Вася", "Юра", "Ана", "Таня", "Татьяна"];
+ar.forEach((item, index, array) => {
+  console.log(`${item} находится на ${index} позиции в  ${array}`);
+});
+
+// или
+arr.forEach(show);
+function show(item) {
+  console.log(item);
+}
